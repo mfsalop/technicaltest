1d2122b389a4ba4c66f4484518230f079bd444ca
mfsalop <mf.sanlop@gmail.com>
2025-03-11 09:19:18 -0500

    github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'r41ais',
   e2e: {
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',  
+      overwrite: false,              
+      html: true,                    
+      json: true,                    
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
